82263b1af17249e581855ee4cc42a4e8d2ec651c
Josleke <josleke@gmail.com>
2024-11-29 01:52:58 +0100

    Added Form presentation

--- src/pages/public/Application/Form/components/Details/index.jsx
+++ src/pages/public/Application/Form/components/Details/index.jsx
@@ -1,11 +1,11 @@
 import { formatAmount, getItem } from "../../../../../../services";
 import Box from "@mui/material/Box";
-import Container from "../../../../../../mui/components/Container";
 import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import { createInitialValues } from "../../../../../../services/createInitialValues";
 import { FormBuilder } from "../../../../../../ui-components/Form/VitalForm/FormBuilder";
 import { createSchema } from "../../../../../../services/createSchema";
+import Grid from "@mui/material/Grid";
 
 const defaultImg = [
   {
@@ -41,64 +41,141 @@ export const Details = () => {
          display={'flex'}
          alignItems={'center'}
          justifyContent={'center'}
+         sx={{ flexGrow: 1 }}
     >
-      <Container className={`!p-2 !bg-[#e6e6e6]`}>
-        <Box display={'flex'} columnGap={'1rem'}>
-          <Box width={'30%'}
-               height={'29rem'}
-               borderRadius={'10px'}
-               paddingBottom={'1rem'}
-               bgcolor={'#f2f2f2'}
+      {/*<Container className={`!p-2 !bg-[#e6e6e6]`}>*/}
+        <Grid container spacing={2}  >
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            data-aos={'fade-up'}
+            data-aos-offset={100}
+            data-aos-duration={600}
           >
-            <Box component={'img'}
-                 src={(file?.logo)? file?.logo : defaultImg[0].casino}
-                 width={1}
-                 height={'12.9rem'}
-                 borderRadius={'10px 10px 0 0'}/>
-            <Box paddingX={'1rem'} >
-              <Typography component={'h1'}
-                          marginY={'0.5rem'}
-                          sx={{
-                            fontWeight: 700,
-                            fontSize: '1.5rem'
-                          }}
+            {/*<Box display={'flex'} columnGap={'1rem'}>*/}
+              <Box width={'100%'}
+                   height={'34rem'}
+                   borderRadius={'10px'}
+                   paddingBottom={'1rem'}
+                   bgcolor={'#f2f2f2'}
               >
-                {file?.name}
-              </Typography>
-              <Typography component={'p'}
-                          marginBottom={'0.4rem'}
-                          paddingBottom={'1.6rem'}
-              >
-                {file?.description} <br />Valid for <span style={{ fontWeight: 700}}>{file?.renewalDuration}</span> days
-              </Typography>
-              <Typography component={'p'} sx={{ fontSize: 'large'}}
-              >
-                <span style={{ fontWeight: 700, color: '#636363', display: 'block'}}>Amount: </span>
-                <span style={{ fontWeight: 700, fontSize: 'xx-large', color: 'red' }}>
+                <Box component={'img'}
+                     src={(file?.logo)? file?.logo : defaultImg[0].casino}
+                     width={1}
+                     height={'12.9rem'}
+                     borderRadius={'10px 10px 0 0'}/>
+                <Box paddingX={'1rem'} >
+                  <Typography component={'h1'}
+                              marginY={'0.5rem'}
+                              sx={{
+                                fontWeight: 700,
+                                fontSize: '1.5rem'
+                              }}
+                  >
+                    {file?.name}
+                  </Typography>
+                  <Typography component={'p'}
+                              marginBottom={'0.4rem'}
+                              paddingBottom={'1.6rem'}
+                  >
+                    {file?.description} <br />Valid for <span style={{ fontWeight: 700}}>{file?.renewalDuration}</span> days
+                  </Typography>
+                  <Typography component={'p'} sx={{ fontSize: 'large'}}
+                  >
+                    <span style={{ fontWeight: 700, color: '#636363', display: 'block'}}>Amount: </span>
+                    <span style={{ fontWeight: 700, fontSize: 'xx-large', color: 'red' }}>
                   ₦ {formatAmount(file?.value)} {file?.feeType?.includes('FLAT')?'':'% of Revenue'}
                 </span>
-              </Typography>
-            </Box>
-          </Box>
-          <Box width={'70%'}
-               borderRadius={'10px'}
-               padding={'1rem'}
-               bgcolor={'#f2f2f2'}>
-            {file?.formTemplate? (
-              <FormBuilder formConfig={file?.formTemplate}
-                           initialValues={initialValue}
-                           validationSchema={schema}
-                           onSubmit={onSubmit} />
-            ) : (
-              <div className={`py-[25%]`}>
-                <h1 className={`text-red-700 text-center text-[2.4rem]`}>
-                  No Document Form is Available!
-                </h1>
-              </div>
-            )}
-          </Box>
-        </Box>
-      </Container>
+                  </Typography>
+                </Box>
+              </Box>
+          </Grid>
+          <Grid
+              item
+              xs={12}
+              sm={6}
+              md={8}
+              data-aos={'fade-up'}
+              data-aos-offset={100}
+              data-aos-duration={600}
+            >
+              <Box width={1}
+                   borderRadius={'10px'}
+                   padding={'1rem'}
+                   bgcolor={'#f2f2f2'}>
+                {file?.formTemplate? (
+                  <FormBuilder formConfig={file?.formTemplate}
+                               initialValues={initialValue}
+                               validationSchema={schema}
+                               onSubmit={onSubmit} />
+                ) : (
+                  <div className={`py-[25%]`}>
+                    <h1 className={`text-red-700 text-center text-[2.4rem]`}>
+                      No Document Form is Available!
+                    </h1>
+                  </div>
+                )}
+              </Box>
+          </Grid>
+        </Grid>
+        {/*<Box display={'flex'} columnGap={'1rem'}>*/}
+        {/*  <Box width={'30%'}*/}
+        {/*       height={'29rem'}*/}
+        {/*       borderRadius={'10px'}*/}
+        {/*       paddingBottom={'1rem'}*/}
+        {/*       bgcolor={'#f2f2f2'}*/}
+        {/*  >*/}
+        {/*    <Box component={'img'}*/}
+        {/*         src={(file?.logo)? file?.logo : defaultImg[0].casino}*/}
+        {/*         width={1}*/}
+        {/*         height={'12.9rem'}*/}
+        {/*         borderRadius={'10px 10px 0 0'}/>*/}
+        {/*    <Box paddingX={'1rem'} >*/}
+        {/*      <Typography component={'h1'}*/}
+        {/*                  marginY={'0.5rem'}*/}
+        {/*                  sx={{*/}
+        {/*                    fontWeight: 700,*/}
+        {/*                    fontSize: '1.5rem'*/}
+        {/*                  }}*/}
+        {/*      >*/}
+        {/*        {file?.name}*/}
+        {/*      </Typography>*/}
+        {/*      <Typography component={'p'}*/}
+        {/*                  marginBottom={'0.4rem'}*/}
+        {/*                  paddingBottom={'1.6rem'}*/}
+        {/*      >*/}
+        {/*        {file?.description} <br />Valid for <span style={{ fontWeight: 700}}>{file?.renewalDuration}</span> days*/}
+        {/*      </Typography>*/}
+        {/*      <Typography component={'p'} sx={{ fontSize: 'large'}}*/}
+        {/*      >*/}
+        {/*        <span style={{ fontWeight: 700, color: '#636363', display: 'block'}}>Amount: </span>*/}
+        {/*        <span style={{ fontWeight: 700, fontSize: 'xx-large', color: 'red' }}>*/}
+        {/*          ₦ {formatAmount(file?.value)} {file?.feeType?.includes('FLAT')?'':'% of Revenue'}*/}
+        {/*        </span>*/}
+        {/*      </Typography>*/}
+        {/*    </Box>*/}
+        {/*  </Box>*/}
+        {/*  <Box width={'70%'}*/}
+        {/*       borderRadius={'10px'}*/}
+        {/*       padding={'1rem'}*/}
+        {/*       bgcolor={'#f2f2f2'}>*/}
+        {/*    {file?.formTemplate? (*/}
+        {/*      <FormBuilder formConfig={file?.formTemplate}*/}
+        {/*                   initialValues={initialValue}*/}
+        {/*                   validationSchema={schema}*/}
+        {/*                   onSubmit={onSubmit} />*/}
+        {/*    ) : (*/}
+        {/*      <div className={`py-[25%]`}>*/}
+        {/*        <h1 className={`text-red-700 text-center text-[2.4rem]`}>*/}
+        {/*          No Document Form is Available!*/}
+        {/*        </h1>*/}
+        {/*      </div>*/}
+        {/*    )}*/}
+        {/*  </Box>*/}
+        {/*</Box>*/}
+      {/*</Container>*/}
     </Box>
   );
 }
