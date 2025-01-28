b6a296f8871dea97f5f14a8c5b63d04e7f84bd44
Josleke <josleke@gmail.com>
2025-01-28 10:20:32 +0100

    Added year selection to report and prod settings

--- src/ui-components/DashBoardReport/StackBarReport/index.jsx
+++ src/ui-components/DashBoardReport/StackBarReport/index.jsx
@@ -10,6 +10,10 @@ import {
 } from "chart.js";
 import { GetDocumentService as getLicenseMetrics } from "../../../services";
 import { Loader } from "../../Loader";
+import { useState } from "react";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
@@ -36,7 +40,9 @@ const options= {
 
 const REPORT_METRIC_URL = process.env.REACT_APP_DOCUMENTS_REPORT_METRICS_URL;
 export const StackBarReport = () => {
-  const { documents, isLoading } = getLicenseMetrics(REPORT_METRIC_URL);
+  const [year, setYear] = useState(2024);
+  const { documents, isLoading }
+    = getLicenseMetrics(`${REPORT_METRIC_URL}?from=${year}-01-01&to=${year}-12-31` );
   const report = documents?.data;
   const data = {
     labels: report?.monthlyReports?.map(month => month?.monthName?.substring(0, 3)),
@@ -68,9 +74,24 @@ export const StackBarReport = () => {
     <div className="flex w-[55%] justify-center rounded-[5px] bg-white-a700 p-2.5 md:w-full md:px-5">
       <div className="mb-4 flex w-full flex-col gap-3.5">
         <div className="flex items-center justify-center sm:flex-col">
-          <Text as="p" className="text-[24px] font-light text-gray-600 md:text-[22px]">
+          <Text as="p" className="text-[24px] font-light text-gray-600 md:text-[22px] pb-8">
             Reports
           </Text>
+          <FormControl className={'!p-0 !ml-2 !h-[70px]'}>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              name={'year'}
+              label={''}
+              defaultValue={'2024'}
+              onChange={(e) => setYear(e.target.value)}
+              className={'!p-0 !w-[100%] !h-[50%] !border-0'}
+                variant={'outlined'}>
+              {[...Array(15).keys()]?.map((option, idx) => (
+                <MenuItem key={idx} value={2024+option}>{2024+option}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
           <div className="flex flex-1 items-center justify-center gap-1 sm:self-stretch">
             <div className="flex flex-1 items-start justify-end px-1.5">
               <div className="h-[12px] w-[12px] rounded-md bg-amber-500" />
