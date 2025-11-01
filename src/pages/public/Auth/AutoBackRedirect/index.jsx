056cdc4f7d042b84d8def51e372978b0dfd7abd5
Josleke <josleke@gmail.com>
2025-11-01 16:52:16 +0100

    Added AutoRedirect back

--- src/pages/public/Auth/AutoBackRedirect/index.jsx
+++ src/pages/public/Auth/AutoBackRedirect/index.jsx
@@ -1,15 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 export const AutoBackRedirect = () => {
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
+    alert(location.pathname);
     setTimeout(() => {
       if (window.history.length > 1) navigate(-1);
       else navigate("/", { replace: true });
     }, 100); // short delay for smoother UX
-  }, [navigate]);
+  }, [navigate, location.pathname]);
 
   return (
     <div className="p-4 text-center text-gray-500">
